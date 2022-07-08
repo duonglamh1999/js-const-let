2ab4505bcb3c4b7840d9ef4541372974382d4d3e
duonglamh1999 <duonglamh1999@gmail.com>
2022-07-08 08:52:54 -0500

    finished

---
+++ app.js
@@ -0,0 +1,9 @@
+const pi =3.14;
+pi=42 //error
+
+/*              var        let          const
+scrope          global     block        block
+reassignable    yes        yes          no
+redeclarable    yes        no           no
+
+hoisting: can access variable before declare */
